d9aa18358bb5fe37c30b6a342e38b489817040c2
Scott Seely <scott.seely@sfsoma.com>
2021-12-21 16:02:14 -0800

    Update queries.sql

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -18,7 +18,7 @@ select dept_manager.dept_number, departments.dept_name, dept_manager.emp_number,
 	inner join employees on dept_manager.emp_number = employees.emp_number;
 	
 
--- 4.  Select the employee number, last name, first name, and department of each employee, ordered by employee number
+-- 4.  Select the department of each employee with the employee number, last name, first name, and department
 
 select employees.emp_number, last_name, first_name, dept_name from employees
 	inner join dept_emp on employees.emp_number = dept_emp.emp_number
@@ -31,7 +31,7 @@ select employees.emp_number, last_name, first_name, dept_name from employees
 select first_name, last_name, birth_date, sex from employees
 	where first_name = 'Hercules' and last_name like 'B%';
 	
--- 6.  Select employee number, last name, first name, and department name of all employees in the Sales department, ordered by employee number
+-- 6.  Select all employees in the sales department, employee number, last name, first name, and department name of all employees in the Sales department
 
 select employees.emp_number, last_name, first_name, dept_name from employees
 	inner join dept_emp on employees.emp_number = dept_emp.emp_number
@@ -47,7 +47,8 @@ select employees.emp_number, last_name, first_name, dept_name from employees
 	where (dept_name = 'Sales' OR dept_name = 'Development')
 	order by employees.emp_number;
 
--- 8.  Count the number of employees, grouped by last name
+-- 8.  Count the number of employees, grouped by last name, order by count
 
 select last_name, count(*) from employees
-	group by last_name;
+	group by last_name
+	order by count;
